53ed98cb96c9da43465efa55366c7682d32f77ec
Shamee Mahmud <sami_safin_mahmud@hotmail.com>
2022-04-16 11:59:28 -0500

    Fixed various runtime errors with missing dependencies and incorrect links.

--- routes/admin/staffadmin.js
+++ routes/admin/staffadmin.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const db = require('../db')
+const db = require('../../db')
 const app = express()
 const fs = require('fs')
 const path = require('path')
-
-const doctoradmin = require('./admin/doctoradmin')
-router.use('/db/admin', doctoradmin)
 
 router.get('/', async (req, res) => {
     try {
--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const db = require('./db')
+const db = require('../db')
 const router = express.Router()
 const fs = require('fs')
 // const port = 80
