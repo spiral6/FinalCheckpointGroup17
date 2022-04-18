3dbe01791894efcd99960d0cf059a87ba753ab72
Shamee Mahmud <sami_safin_mahmud@hotmail.com>
2022-04-18 12:08:42 -0500

    Honestly I don't even remember anymore.

--- routes/doctorroute.js
+++ routes/doctorroute.js
@@ -21,13 +21,98 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/findpatient', async (req, res) => {
+    try{
+        if(req.cookies['doc_id']){
+            res.sendFile(path.join(__dirname + "/../html/doctor/doctorfindpatient.html"));
+        } else if(req.cookies['admin']){
+            res.redirect('/admin');
+        } else {
+            res.redirect('/');
+        }
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+router.get('/viewpayroll', async (req, res) => {
+    try{
+        if(req.cookies['doc_id']){
+            res.sendFile(path.join(__dirname + "/../html/doctor/doctorviewpayroll.html"));
+        } else if(req.cookies['admin']){
+            res.redirect('/admin');
+        } else {
+            res.redirect('/');
+        }
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+router.get('/viewschedule', async (req, res) => {
+    try{
+        if(req.cookies['doc_id']){
+            res.sendFile(path.join(__dirname + "/../html/doctor/doctorviewschedule.html"));
+        } else if(req.cookies['admin']){
+            res.redirect('/admin');
+        } else {
+            res.redirect('/');
+        }
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+router.get('/createprescription', async (req, res) => {
+    try{
+        if(req.cookies['doc_id']){
+            res.sendFile(path.join(__dirname + "/../html/doctor/doctorcreateprescription.html"));
+        } else if(req.cookies['admin']){
+            res.redirect('/admin');
+        } else {
+            res.redirect('/');
+        }
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+router.get('/viewprofile', async (req, res) => {
+    try{
+        if(req.cookies['doc_id']){
+            res.sendFile(path.join(__dirname + "/../html/doctor/doctorviewprofile.html"));
+        } else if(req.cookies['admin']){
+            res.redirect('/admin');
+        } else {
+            res.redirect('/');
+        }
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+router.get('/editprofile', async (req, res) => {
+    try{
+        if(req.cookies['doc_id']){
+            res.sendFile(path.join(__dirname + "/../html/doctor/doctoreditprofile.html"));
+        } else if(req.cookies['admin']){
+            res.redirect('/admin');
+        } else {
+            res.redirect('/');
+        }
+    } catch (err) {
+        console.log(err);
+    }
+})
+
 // Search for patient
 router.get('/db/findPatient', async (req, res) => {
     try {
-        let searchName = "%" + req.query.doctor + "%";
-        const result = await db.pool.query("SELECT pat_name, pat_email, pat_phone FROM PatientTable WHERE pat_name LIKE ? ORDER BY staff_name ASC;",[searchName]);
+        // console.log(req.query.q);
+        let searchName = "%" + req.query.q + "%";
+        const result = await db.pool.query("SELECT pat_id, pat_name, pat_email, pat_phone FROM PatientTable WHERE pat_name LIKE ? ORDER BY pat_name ASC;",[searchName]);
         
-        console.log(result);
+        // console.log(result);
         res.send(result);
     } catch (err) {
         console.error(err);
@@ -37,9 +122,10 @@ router.get('/db/findPatient', async (req, res) => {
 // Get patient information
 router.get('/db/patientInfo', async (req, res) => {
     try {
-        const result = await db.pool.query("SELECT pat_name,pat_sex,pat_email,pat_phone,pat_DoB,pat_height,pat_weight,pat_allergy,pat_insurance,pat_address FROM PatientTable WHERE pat_id=?;",[req.body.pat_id]);
+        // console.log(req.query)
+        const result = await db.pool.query("SELECT * FROM PatientTable WHERE pat_id=?;",[req.query.pat_id]);
         
-        console.log(result);
+        // console.log(result);
         res.send(result);
     } catch (err) {
         console.error(err);
@@ -105,6 +191,34 @@ router.put('/db/prescription', async (req, res) => {
 router.get('/db/appointment', async (req, res) => {
     try {
         const result = await db.pool.query("SELECT app_id,app_time,LocationTable.loc_name,PatientTable.pat_name FROM (((AppointmentTable INNER JOIN LocationTable ON AppointmentTable.loc_id=LocationTable.loc_id) INNER JOIN StaffTable ON AppointmentTable.doc_id=StaffTable.staff_id) INNER JOIN PatientTable ON AppointmentTable.pat_id=PatientTable.pat_id) WHERE doc_id=?;",[
+            req.cookies['doc_id']
+        ]);
+        
+        console.log(result);
+        res.send(result);
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+// View payroll
+router.get('/db/payroll', async (req, res) => {
+    try {
+        const result = await db.pool.query("SELECT staff_name, staff_salary FROM StaffTable WHERE staff_id=?",[
+            req.cookies['doc_id']
+        ]);
+        
+        console.log(result);
+        res.send(result);
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+// View schedule
+router.get('/db/schedule', async (req, res) => {
+    try {
+        const result = await db.pool.query("SELECT StaffTable.staff_name,schedule_workday,LocationTable.loc_name FROM ((ScheduleTable INNER JOIN LocationTable ON ScheduleTable.loc_id=LocationTable.loc_id) INNER JOIN StaffTable ON ScheduleTable.staff_id=StaffTable.staff_id) WHERE ScheduleTable.staff_id=?;",[
             req.cookies['doc_id']
         ]);
         
