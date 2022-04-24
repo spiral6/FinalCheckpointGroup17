ddbd71e746007aaeee3e0925d8e588e8a3687c21
Shamee Mahmud <sami_safin_mahmud@hotmail.com>
2022-04-24 18:01:40 -0500

    Added medicine drop down to table.

--- js/doctor/doctorprescription.js
+++ js/doctor/doctorprescription.js
@@ -105,6 +105,7 @@ selectedRow = null;
   });
 
   refreshPrescriptionTable();
+  getMedicineNames();
 }
 
 async function AddPrescription(JSONObject){
@@ -252,4 +253,29 @@ function updatePrescriptionTableUI(body){
       "<td>" + "<button class='btn btn-lg btn-primary btn-block' onclick='removePrescription(this);''><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash' viewBox='0 0 16 16'><path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z'/><path fill-rule='evenodd' d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z'/></svg></button>" + "</td>" +
       "</tr>");
   }
+}
+
+async function getMedicineNames(){
+  const request = await fetch(window.location.origin + '/doctor/db/medicine',  {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      // body: JSON.stringify(JSONObject)
+    }).then(response => {
+      if (!response.ok) {
+        response.text().then(err => {
+          alert(`HTTP error: ${response.status} \n${JSON.parse(err).text}`);
+        })  
+      }
+      return response.text();
+    }).then(body => {
+      // alert("Successfully retrieved Doctors!")
+      console.log(JSON.parse(body));
+      body = JSON.parse(body);
+      for(const row in body){
+        // console.log(row.med_name);
+        $("#med_name").append("<option>" + body[row].med_name + "</option>");
+      }
+    });
 }
